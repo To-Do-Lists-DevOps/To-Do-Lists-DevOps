1cf40ab83a91431cb77496936ef451ed45164839
fndsjy <fendysanjaya40@gmail.com>
2023-10-17 10:49:16 +0700

    Adding The Atomic Design and Routes

---
+++ src/component/atoms/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <div className="container-fluid">
+            <p className="text-[12em] font-bold">Tasks</p>
+        </div>
+    );
+}
+
+export default Header;
