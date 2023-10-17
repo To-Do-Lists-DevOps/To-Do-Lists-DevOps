9cb81239d6db2f1bf89b0de0746490c36dd26bee
fndsjy <fendysanjaya40@gmail.com>
2023-10-17 16:31:19 +0700

    Adding The Input Tasks Form

---
+++ src/component/atoms/PlusIcon.jsx
@@ -0,0 +1,9 @@
+const PlusIcon = () => {
+    return(
+        <div>
+            <i className="px-4 fa-solid fa-plus fa-beat-fade text-2xl text-[#778BDD]"></i>
+        </div>
+    );
+}
+
+export default PlusIcon;
